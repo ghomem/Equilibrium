6107b51372ec1f50b212e86b0e2b36e4b83f98af
Gustavo Homem <gustavo@angulosolido.pt>
2013-10-21 01:54:40 +0100

    store matrix definitions in a separate file

--- octave/input.m
+++ octave/input.m
@@ -1,31 +1,3 @@
-% Equilibrium of simpathies
-%
-% numerical solution of ODE system for the 10 body problem in Economics.
-
-clear all;
-
-% parameters
-
-global Sysdim=10;
-global C;
-interval = 3;  % time interval
-nrpoints= floor(interval)*100;  % step for ODE solving, 100 pts per time units seems enough
-confidence = 1; % affects both time to equilibrium and GDP value
-ymargin = 10; % just a visual margin for plots
-saveplots = 1;
-
-% individual spending tendency
-
-Ci = [ 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ];
-
-% actual spending tendency
-
-ACi = Ci * confidence;
-
-% initial condition
-
-V0=[ 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 ];
-
 % several matrix possibilities
 % mutual simpathies: we must have Cii = 0
 
@@ -45,7 +17,7 @@ S0 = [  0, 0, 0, 0, 0, 0, 0, 0, 0, 0  ;
 % uniform matrix with unitary sympathy
 
 S1 = [  0, 1, 1, 1, 1, 1, 1, 1, 1, 1  ;
-	1, 0, 1, 1, 1, 1, 1, 1, 1, 1  ;
+	2, 0, 1, 1, 1, 1, 1, 1, 1, 1  ;
 	1, 1, 0, 1, 1, 1, 1, 1, 1, 1  ;
 	1, 1, 1, 0, 1, 1, 1, 1, 1, 1  ;
 	1, 1, 1, 1, 0, 1, 1, 1, 1, 1  ;
@@ -198,7 +170,7 @@ S9 = [  0, 0, 0, 0, 0, 0, 0, 0, 0, 0  ;
 
 low=0.1;
 high=1;
-% increasing this simpathy will decrease the GDP (counter example!)
+% increasing this simpathy will decrease the equilibrium GDP (counter example!)
 delta=0.0;
 
 S9b = [  0   ,        low , low, low, low,    low, low,  low,   low, low ;
@@ -313,131 +285,3 @@ A1 = [  0, 1, 1, 1, 1, 1, 1, 1, 1, 1  ;
 	1, 1, 1, 1, 1, 1, 1, 0, 1, 1  ;
 	1, 1, 1, 1, 1, 1, 1, 1, 0, 1  ;
 	1, 1, 1, 1, 1, 1, 1, 1, 1, 0 ];
-
-% **** Main script ****
-
-% choose the simpathy and awareness matrices for this simulation
-
-Sij = S9b;
-Aij = A1;
-
-% initialize Cij
-
-for i=1:Sysdim
-  for j=1:Sysdim
-    C(i,j) = ACi(i) * Sij(i,j) * Aij(i,j);
-  endfor
-endfor
-
-% compute matrix M and its eigenvalues
-% note - the matrix is not used for the ODE solving as we are using the scalar equations for xdot on the deq function
-
-D=zeros(Sysdim,1);
-for i=1:Sysdim
-  for j=1:Sysdim
-    D(i) = D(i) + C(i,j);
-  endfor
-endfor
-
-for i=1:Sysdim
-  for j=1:Sysdim
-    if j != i
-      M(i,j) = C(j,i);
-    else
-      M(i,j) = -D(i);
-    endif
-  endfor
-endfor
-
-%disp(M);
-
-% the eigenvalues must be zero and a set of numbers with negative real part; zero may appear as a very small number in some cases
-
-disp("Eigenvalues: ");
-eig(M)
-
-t=linspace(0,interval,nrpoints); % time frames
-V=lsode("deq",V0,t); % ODE solver - obtains the saved value function V, uses matrix C (global var)
-
-% plot values, configure y axis for better appearance
-
-figure(1);
-plot(t, V(:,1), t, V(:,2), t, V(:,3), t, V(:,4),t, V(:,5),t, V(:,6),t, V(:,7),t, V(:,8),t, V(:,9),t, V(:,10) );
-title("Wealth acumulation");
-mylimit = max ( [ V(nrpoints,1) V(nrpoints,2) V(nrpoints,3) V(nrpoints,4) V(nrpoints,5) V(nrpoints,6) V(nrpoints,7) V(nrpoints,8) V(nrpoints,9) V(nrpoints,10) ] ) + ymargin;
-ylim([ 0 mylimit ]);
-
-% save the plot to a file
-if saveplots == 1
-  print("output/wealth-plot.png","-dpng");
-endif
-
-
-% now let's calculate the personal GDPs, ie the total flows
-
-g = zeros(nrpoints,Sysdim);
-
-for h=1:nrpoints
-  for i=1:Sysdim
-    sum1 = 0; 
-    sum2 = 0;
-    for j=1:Sysdim
-      sum1 = sum1 + C(i,j)*V(h,i); %outward flow - expenditure
-    endfor
-    g(h,i) = sum1 ;
-  endfor
-endfor
-
-%figure(2);
-%plot(t, g(:,1), t, g(:,2), t, g(:,3), t, g(:,4),t, g(:,5),t, g(:,6),t, g(:,7),t, g(:,8),t, g(:,9),t, g(:,10) );
-
-% we can now calculate the total GDP
-
-G = zeros(nrpoints,1);
-
-for h=1:nrpoints
-  for i=1:Sysdim
-    G(h) = G(h) + (g(h,i) / 2); % discount the double counting
-  endfor
-endfor
-
-% plot total GDP , configure y axis for better appearance
-
-figure(3);
-plot(t, G);
-title("Total GDP");
-mylimit = G(1) + 5*ymargin;
-ylim([ 0 mylimit ]);
-
-% save the plot to a file
-if saveplots == 1
-  print("output/GDP-plot.png","-dpng");
-endif
-
-% calculate total simpathy
-
-TS=0;
-
-for i=1:Sysdim
-  for j=1:Sysdim
-    TS = TS + Sij(i,j); 
-  endfor
-endfor
-
-disp("Total simpathy: "), disp(TS);
-disp("Total GDP: "), disp(G(nrpoints));
-disp("GDP per capita: "), disp(G(nrpoints)/Sysdim);
-
-% calculate Gini coefficient for the Vis
-
-aux = zeros(Sysdim,1);
-% V is tranposed with symbol '
-GC = ginicoeff(V(nrpoints,:)',1,false);
-disp("Gini coefficient: "), disp(GC);
-
-% gini function validation
-
-%test1 = [ 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 ];
-%test2 = [ 1 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 ];
-%GC = ginicoeff(test1,1,false)
-%GC = ginicoeff(test2,1,false)
